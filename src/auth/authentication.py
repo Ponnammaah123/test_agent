758a66a8cbb4c80dfdf601cdbf62ba26054aa7e5
Ponnammaah123 <vidyaappachira@gmail.com>
2025-11-12 19:03:27 +0530

    checkpoint-2

--- src/auth/authentication.py
+++ src/auth/authentication.py
@@ -1,14 +1,16 @@
-from fastapi import Depends, HTTPException, Header
+from fastapi import HTTPException, Header
 import jwt
 import os
 import datetime
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
-from dotenv import load_dotenv
-
+import requests
 # Configuration
 ALGORITHM = "RS256"
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
 
 async def authenticate_user(authorization: str = Header(None)):
     """
@@ -25,21 +27,46 @@ async def authenticate_user(authorization: str = Header(None)):
     """
     if not authorization:
         raise HTTPException(status_code=401, detail="Authorization header is missing")
+
     # Check for 'Bearer ' prefix
     if not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Invalid Authorization header format")
+        raise HTTPException(
+            status_code=401, detail="Invalid Authorization header format"
+        )
+
     token = authorization[7:]  # Strip the 'Bearer ' prefix
+
     try:
         # Load the public key from environment variables
         public_key = load_public_key_from_env("SECRET_KEY")
+
         # Decode and validate the token
-        payload = jwt.decode(token, public_key, algorithms=[ALGORITHM], options={"verify_exp": True},leeway=30)
+        payload = jwt.decode(
+            token,
+            public_key,
+            algorithms=[ALGORITHM],
+            options={"verify_exp": True},
+            leeway=30,
+        )
+
         # Validate required fields
-        required_fields = ["type", "jti", "UserInfo", "appKey", "services", "appName", "industry", "exp"]
+        required_fields = [
+            "type",
+            "jti",
+            "UserInfo",
+            "appKey",
+            "services",
+            "appName",
+            "industry",
+            "exp",
+        ]
+        print(f"------------111111111-------")
         for field in required_fields:
             if field not in payload:
-                raise HTTPException(status_code=401, detail=f"Missing required field: {field}")
-
+                raise HTTPException(
+                    status_code=401, detail=f"Missing required field: {field}"
+                )
+        print(f"--------payload----", payload)
         # Check `type`
         if payload["type"] != "at":
             raise HTTPException(status_code=401, detail="Invalid token type")
@@ -63,10 +90,12 @@ async def authenticate_user(authorization: str = Header(None)):
 
         # Validate `appId` and `appName`
         if not payload.get("appId") or not payload.get("appName"):
-            raise HTTPException(status_code=401, detail="Invalid appId or appName in token")
+            raise HTTPException(
+                status_code=401, detail="Invalid appId or appName in token"
+            )
 
         # Validate `iat` and `exp`
-        current_time = datetime.datetime.utcnow().timestamp()
+        current_time = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
         if payload["exp"] < current_time:
             raise HTTPException(status_code=401, detail="Token has expired")
         # Return the payload if all validations pass
@@ -78,6 +107,7 @@ async def authenticate_user(authorization: str = Header(None)):
         raise HTTPException(status_code=401, detail=f"Invalid token: {str(e)}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Token validation error: {str(e)}")
+
 
 def load_public_key_from_env(env_var_name: str) -> serialization.PublicFormat:
     """
@@ -92,24 +122,20 @@ def load_public_key_from_env(env_var_name: str) -> serialization.PublicFormat:
     Raises:
         ValueError: If the key cannot be deserialized or is in an incorrect format.
     """
-    dotenv_path = os.path.join(os.path.dirname(__file__), "../config/.env")
-
-        # Load the environment variables from the .env file
-    load_dotenv(dotenv_path)
     key_data = os.getenv(env_var_name)
     if not key_data:
-        raise ValueError(f"The environment variable '{env_var_name}' is not set or empty.")
+        raise ValueError(
+            f"The environment variable '{env_var_name}' is not set or empty."
+        )
 
     # Replace escaped line breaks with actual line breaks
     key_data = key_data.replace("\\n", "\n")
 
     try:
         public_key = serialization.load_pem_public_key(
-            key_data.encode(),
-            backend=default_backend()
+            key_data.encode(), backend=default_backend()
         )
         # print("Public key loaded successfully!")
         return public_key
     except Exception as e:
         raise ValueError(f"Failed to load public key: {e}")
-
